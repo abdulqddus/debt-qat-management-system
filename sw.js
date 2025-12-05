708215369460c5cb28e634667ef9b81969c7f2e4
abdulqddus <bdalqdwsalrshy4@gmail.com>
2025-12-06 02:21:26 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Service Worker بسيط
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
